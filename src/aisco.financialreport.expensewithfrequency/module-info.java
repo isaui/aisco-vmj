d3c3d9d6bf73686c20b9e73963631d2bf4016006
austinsusanto <austinsusanto@gmail.com>
2026-04-03 23:18:32 +0700

    generate yayasanrse

---
+++ src/aisco.financialreport.expensewithfrequency/module-info.java
@@ -0,0 +1,6 @@
+module aisco.financialreport.expensewithfrequency {
+    exports aisco.financialreport.expensewithfrequency;
+    requires aisco.financialreport.core;
+    requires aisco.financialreport.expense;
+    requires aisco.financialreport.frequency;
+}
---
+++ src/aisco.financialreport.incomewithfrequency/module-info.java
@@ -0,0 +1,6 @@
+module aisco.financialreport.incomewithfrequency {
+    exports aisco.financialreport.incomewithfrequency;
+    requires aisco.financialreport.core;
+    requires aisco.financialreport.income;
+    requires aisco.financialreport.frequency;
+}

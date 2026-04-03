1a787cf8597d26988e50a5e65c5cba8c21d64b9a
Maurice Yang <mauriceyangmy28@gmail.com>
2026-04-03 23:29:41 +0700

    feat: add GoodSchool product

---
+++ src/aisco.product.goodschool/module-info.java
@@ -0,0 +1,14 @@
+module aisco.product.charityschool {
+    requires aisco.program.core;
+    requires aisco.financialreport.core;
+    requires aisco.donation.core;
+
+    requires aisco.program.activity;
+    requires aisco.program.operational;
+    requires aisco.program.scholarship;
+
+    requires aisco.financialreport.income;
+    requires aisco.financialreport.expense;
+
+    requires aisco.donation.pgateway;
+}
